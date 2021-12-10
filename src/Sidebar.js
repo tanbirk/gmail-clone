edf62dcb770a19934f428dcfd07b371de23a0687
tanbirk <tanvir.kashyap879@gmail.com>
2021-12-10 18:27:12 +0530

    four

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,7 +5,11 @@ import AddIcon from '@material-ui/icons/Add'
 import SidebarOption from './SidebarOption'
 import InboxIcon from "@material-ui/icons/Inbox"
 import StarIcon from "@material-ui/icons/Star"
-
+import  AccessTimeIcon  from '@material-ui/icons/AccessTime'
+import  LabelImportantIcon  from '@material-ui/icons/LabelImportant'
+import  NearMeIcon  from '@material-ui/icons/NearMe'
+import  NoteIcon  from '@material-ui/icons/Note'
+import  ExpandMoreIcon  from '@material-ui/icons/ExpandMore'
 
 
 function Sidebar() {
@@ -22,8 +26,14 @@ function Sidebar() {
             selected ={true}
              />
             <SidebarOption Icon ={StarIcon} title="Starred" number={69} />
+            <SidebarOption Icon ={AccessTimeIcon} title="Snooze" number={69} />
+            <SidebarOption Icon ={LabelImportantIcon} title="Important" number={29} />
+            <SidebarOption Icon ={NearMeIcon} title="Sent" number={64} />
+            <SidebarOption Icon ={NoteIcon} title="Drafts" number={69} />
+            <SidebarOption Icon ={ExpandMoreIcon} title="More" number={420} />
              </div>
     
 }
 
 export default Sidebar
+ 
