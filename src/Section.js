4a18e998484cbad4b547cfb12ee78fda3a22602d
tanbirk <tanvir.kashyap879@gmail.com>
2021-12-10 22:12:21 +0530

    8

--- src/Section.js
+++ src/Section.js
@@ -6,8 +6,10 @@ function Section({ Icon , title, color , selected}) {
         <div className={`section ${selected && "section--selected"}`}
             style={{
                 borderBottom: `3px solid ${color}`,
-                color: `${selected && color}`
+                color: `${selected && color}`,
             }}>
+            <Icon />
+            <h4>{title}</h4>
         </div>
     )
 }
